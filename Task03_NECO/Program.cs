2771ffe4ef1ffa8d115bfdabc823eb325084a94e
MoriJellyfish <morijellyfish@gmail.com>
2022-05-17 22:27:13 +0900

    回答手順

--- Task03_NECO/Program.cs
+++ Task03_NECO/Program.cs
@@ -30,6 +30,7 @@ namespace Task03_NECO
             array = list.ToArray();
 
             //output
+            //変な解き方したくなってこんなことしたけどそこまで変でもないね、それぞれの大きさで引いて余りを取るだけ
             foreach (int i in array)
             {
                 int a = 1000 - i;
@@ -40,17 +41,17 @@ namespace Task03_NECO
         }
         static void Task02()
         {
-            //
+            //input
             string[] linestr = Console.ReadLine().Split(' ');
             int n = int.Parse(linestr[0]);
             int m = int.Parse(linestr[1]) - 1;
             int[] coins = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
-            //
+            //DPってやつかこれが
             int[] mins = new int[n + 1];
             for (int i = 0; i < mins.Length; i++)
             {
-                mins[i] = 99999;
+                mins[i] = 99999;//めちゃくちゃ枚数が必要ってことにすることで避けるよ
             }
 
             foreach (int c in coins)
@@ -66,7 +67,7 @@ namespace Task03_NECO
             {
                 min = n + 1;
                 foreach (int c in coins)
-                {
+                {//各コインで来るっと回して最小比較をするよ
                     if (i - c >= 0)
                     {
                         mins[i] = Math.Min(mins[i], mins[i - c] + 1);
@@ -74,6 +75,7 @@ namespace Task03_NECO
 
                 }
             }
+            //下から計算してってたどり着いたnは最小になる、多分
             Console.WriteLine(mins[n]);
         }
 
